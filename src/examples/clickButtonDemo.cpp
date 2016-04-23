97ff9f685f2065aa95eca9e70a88aefff46213b6
Michael Teeuw <michael@xonaymedia.nl>
2016-04-23 08:37:23 +0200

    Update example.

--- src/examples/clickButtonDemo.cpp
+++ src/examples/clickButtonDemo.cpp
@@ -2,21 +2,21 @@
 /* ClickButton library demo
 
   OUtput a message on Serial according to different clicks on one button.
-  
+
   Short clicks:
 
-    Single click - 
-    Double click - 
-    Triple click - 
-    
+    Single click -
+    Double click -
+    Triple click -
+
   Long clicks (hold button for one second or longer on last click):
-    
-    Single-click - 
-    Double-click - 
-    Triple-click - 
+
+    Single-click -
+    Double-click -
+    Triple-click -
 
   2010, 2013 raron
- 
+
  GNU GPLv3 license
 */
 #include "clickButton/clickButton.h"
@@ -26,9 +26,12 @@
 const int buttonPin1 = 4;
 ClickButton button1(buttonPin1, LOW, CLICKBTN_PULLUP);
 
-// Button results 
+// Button results
 int function = 0;
 
+void releaseCallback() {
+	Serial.println("The button is released!");
+}
 
 void setup()
 {
@@ -40,6 +43,9 @@ void setup()
   button1.debounceTime   = 20;   // Debounce timer in ms
   button1.multiclickTime = 250;  // Time limit for multi clicks
   button1.longClickTime  = 1000; // time until "held-down clicks" register
+
+  // Set the callback for releasing a longpress.
+  button1.setReleaseCallback(releaseCallback);
 }
 
 
@@ -50,7 +56,7 @@ void loop()
 
   // Save click codes in LEDfunction, as click codes are reset at next Update()
   if (button1.clicks != 0) function = button1.clicks;
-  
+
   if(button1.clicks == 1) Serial.println("SINGLE click");
 
   if(function == 2) Serial.println("DOUBLE click");
@@ -62,7 +68,7 @@ void loop()
   if(function == -2) Serial.println("DOUBLE LONG click");
 
   if(function == -3) Serial.println("TRIPLE LONG click");
-  
+
   function = 0;
   delay(5);
 }
