77557b7a24ee1cb83403aa5b6d967dbff3b7fa46
alexgrauer <alexgrauer@gmail.com>
2016-09-20 14:24:58 -0300

    Update clickButtonDemo.cpp

--- firmware/examples/clickButtonDemo.cpp
+++ firmware/examples/clickButtonDemo.cpp
@@ -1,4 +1,3 @@
-
 /* ClickButton library demo
 
   OUtput a message on Serial according to different clicks on one button.
@@ -11,9 +10,11 @@
     
   Long clicks (hold button for one second or longer on last click):
     
-    Single-click - 
-    Double-click - 
-    Triple-click - 
+    Single LONG click - 
+    Double LONG click - 
+    Triple LONG click -
+    
+    button2 will report release
 
   2010, 2013 raron
  
@@ -21,19 +22,23 @@
 */
 #include "clickButton/clickButton.h"
 
-
 // the Button
-const int buttonPin1 = 4;
-ClickButton button1(buttonPin1, LOW, CLICKBTN_PULLUP);
+const int buttonPin1 = 0;
+const int buttonPin2 = 1;
+//ClickButton button1(buttonPin1, LOW, CLICKBTN_PULLUP);
+ClickButton button1(buttonPin1, HIGH, CLICKBTN_PULLDOWN, false);
+ClickButton button2(buttonPin2, LOW, CLICKBTN_PULLUP, true);
 
 // Button results 
 int function = 0;
+int function2 = 0;
 
 
 void setup()
 {
   Serial.begin(9600);
-  pinMode(D4, INPUT_PULLUP);
+  //pinMode(D0, INPUT_PULLUP);
+  pinMode(D0, INPUT);
 
   // Setup button timers (all in milliseconds / ms)
   // (These are default if not set, but changeable for convenience)
@@ -47,22 +52,49 @@ void loop()
 {
   // Update button state
   button1.Update();
+  button2.Update();
 
   // Save click codes in LEDfunction, as click codes are reset at next Update()
   if(button1.clicks != 0) function = button1.clicks;
   
-  if(function == 1) Serial.println("SINGLE click");
+  if(function == 1) Serial.println("SINGLE click 1");
 
-  if(function == 2) Serial.println("DOUBLE click");
+  if(function == 2) Serial.println("DOUBLE click 1");
 
-  if(function == 3) Serial.println("TRIPLE click");
+  if(function == 3) Serial.println("TRIPLE click 1");
 
-  if(function == -1) Serial.println("SINGLE LONG click");
+  if(function == -1) Serial.println("SINGLE LONG click 1");
 
-  if(function == -2) Serial.println("DOUBLE LONG click");
+  if(function == -2) Serial.println("DOUBLE LONG click 1");
 
-  if(function == -3) Serial.println("TRIPLE LONG click");
+  if(function == -3) Serial.println("TRIPLE LONG click 1");
+  
+  if(function >= 100){
+    Serial.print("Release 1: ");
+     Serial.println(function);
+  }
   
   function = 0;
+  
+  if(button2.clicks != 0) function2 = button2.clicks;
+  
+  if(function2 == 1) Serial.println("SINGLE click 2");
+
+  if(function2 == 2) Serial.println("DOUBLE click 2");
+
+  if(function2 == 3) Serial.println("TRIPLE click 2");
+
+  if(function2 == -1) Serial.println("SINGLE LONG click 2");
+
+  if(function2 == -2) Serial.println("DOUBLE LONG click 2");
+
+  if(function2 == -3) Serial.println("TRIPLE LONG click 2");
+  
+  if(function2 >= 100){
+    Serial.print("Release 2: ");
+     Serial.println(function2);
+  }
+  
+  function2 = 0;
   delay(5);
 }
